f22de6f91011722b84aa9ffdad3f8c74aaab499a
lijinhui <musiteam@musiiot.top>
2018-04-20 01:27:20 +0800

    0420 01:24 note coumpter

--- app/wechat/model/CodeModel.php
+++ app/wechat/model/CodeModel.php
@@ -13,12 +13,13 @@ class CodeModel extends Model
 	protected $table = 'cmf_code';
 
 	public static function creatCode($phone, $code){
-            $this->data([
-                  'phone' => $phone,
-                  'code' => $code,
-                  'time' => ''
-            ]);
-            $this->save();
+		$cd = new CodeModel();
+        $cd->data([
+              'phone' => $phone,
+              'code' => $code,
+              'time' => ''
+        ]);
+        $cd->save();
       }
 }
 
