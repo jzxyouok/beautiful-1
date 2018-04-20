bdb7d8d9c3851a3770f7e70f500e1dbc0dcb9eea
lijinhui <musiteam@musiiot.top>
2018-04-20 09:13:40 +0800

    0420 09:02 note coumpter checkCode completed

--- app/wechat/model/CodeModel.php
+++ app/wechat/model/CodeModel.php
@@ -25,7 +25,7 @@ class CodeModel extends Model
 
     public static function getCode($phone){
     	$cd = new CodeModel();
-    	return $phone;ss
+    	return $phone;
       	$user = $cd->get(function($query){
       		$query->where('phone', $phone)->limit(1)->order('time', 'desc');;
       	});
