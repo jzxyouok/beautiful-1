b664c4775d27d20fc9a2762d5828ac5e7c5dc225
lijinhui <musiteam@musiiot.top>
2018-04-24 23:53:17 +0800

    0424 23:39 note coumpter

--- app/portal/controller/AdminArticleController.php
+++ app/portal/controller/AdminArticleController.php
@@ -135,7 +135,7 @@ class AdminArticleController extends AdminBaseController
             Db::name('portal_post')
                 ->where('id', $portalPostModel->id)
                 ->update(['belong' => $belong]);
-
+            echo $portalPostModel->id;
             $this->success('添加成功!', url('AdminArticle/edit', ['id' => $portalPostModel->id]));
         }
 
